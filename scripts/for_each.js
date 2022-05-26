9be1d520550a2dd5b7acbb830fb04c2035c410ee
Jose Cossi <josecossib@gmail.com>
2022-05-25 22:57:29 -0300

    practice ready

--- scripts/for_each.js
+++ scripts/for_each.js
@@ -1,9 +1,24 @@
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 numbers.forEach((number, i) => {
-    console.log(number, i)
-})
+  console.log(number, i);
+});
 
-const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+const countries = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
 countries.forEach((country, i, arr) => {
-    console.log(i, country.toUpperCase())
-})
+  console.log(i, country.toUpperCase());
+});
+
+const nums = [1, 2, 3, 4, 5];
+for (const number of nums) {
+  console.log(number);
+}
+
+// const countries = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
+// for (const country of countries) {
+//   console.log(country.toUpperCase());
+// }
+
+const paises = ["VE", "CA", "UK", "RU"];
+paises.forEach((pais, i) => {
+  console.log(pais.toLowerCase());
+});
